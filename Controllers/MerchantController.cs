68d746e044931789067b9614da9847e02a6c3c50
Lmhyeon129 <lmhyeon129@gmail.com>
2019-07-23 13:54:36 +0900

    initial create

---
+++ Controllers/MerchantController.cs
@@ -0,0 +1,12 @@
+using System.Collections.Generic;
+using Microsoft.AspNetCore.Mvc;
+
+namespace donation_MERCHANT.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class MerchantController : ControllerBase
+    {
+        // GET Get(id) Post(model) Put(id, model) Delete(id)
+    }
+}
